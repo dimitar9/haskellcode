fed2a5132c5902237d8471e46551a67188ee1b16
dimitar9 <dimitar9909@gmail.com>
2013-10-12 00:51:03 -0400

    simple record

---
+++ simplerecord.hs
@@ -0,0 +1,25 @@
+data Person = Person {
+                        firstname::String
+                    ,   lastName :: String
+                    ,   age :: Int
+                    ,   height :: Float
+                    ,   phoneNumber :: String
+                    ,   flavor :: String
+}deriving(Show)
+
+
+    
+    
+guy = Person "Buddy" "Finklestein" 43 184.2 "526-1234" "Vanilla"
+
+    
+    
+    
+    
+    
+    
+    
+main = do
+    print (guy)
+    print (firstname guy)
+    print (flavor guy)
