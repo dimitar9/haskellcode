237a337be9ac81d6b6d0ae5a14ce72a82cb79dba
dimitar9 <dimitar9909@gmail.com>
2013-10-13 12:25:41 -0400

    type

---
+++ type_synonyms.hs
@@ -0,0 +1,11 @@
+import Data.Map
+type PhoneNumber = String
+type Name = String
+type PhoneBook =[(Name,PhoneNumber)]
+
+--inPhoneBook :: Name -> PhoneNumber -> PhoneBook -> Bool
+--inPhoneBook name pnumber pbook = (name,number) `elem` pbook
+
+type IntMap = Map Int
+main = do
+    print "hello"
