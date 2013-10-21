f0a1a8fcee5cd104418abe63485a3f06460f3a13
dimitar9 <dimitar9909@gmail.com>
2013-10-20 22:38:58 -0400

    io

---
+++ getlines.hs
@@ -0,0 +1,10 @@
+main = do
+    line <- getLine
+    if null line
+        then return ()
+    else do
+        putStrLn $ reverseWord line
+        main
+
+reverseWord:: String -> String
+reverseWord = unwords . map reverse . words
