e69a294c23bf6ea6f100363536f76a49148e20a8
dimitar9 <dimitar0090@gmail.com>
2013-10-23 12:21:06 -0400

    functor

--- input.hs
+++ input.hs
@@ -1,4 +1,12 @@
+import Data.Char
+
 main = do
-    putStrLn "Hello, what's your name?"
-    name <- getLine
-    putStrLn ("hey" ++ name ++ ", you rock!")
+    putStrLn "Hello, what's your first name?"
+    firstName <- getLine
+    putStrLn "What is your last name?"
+    lastName <- getLine
+    let bigFirstName = map toUpper firstName
+        bigLastName  = map toUpper lastName
+    putStrLn $ "hey " ++ bigFirstName ++ " " ++ bigLastName ++ ", how are you?" 
+
+
