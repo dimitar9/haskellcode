c45279807666b13450dc155502d859b74d86cff4
dimitar9 <dimitar9909@gmail.com>
2013-10-24 22:49:33 -0400

    -

--- functor2.hs
+++ functor2.hs
@@ -2,7 +2,9 @@ main = do line <- fmap reverse getLine
           putStrLn $ "You said " ++ line ++ " backwards!"  
           putStrLn $ "Yes, you really said" ++ line ++ " backwards!" 
 
-        --  putStrLn <- fmap (replicate 3) [1,2,3,4]
+          putStrLn $ show $ fmap (replicate 3) [1,2,3,4]
+
+          print  $ fmap (replicate 3) [1,2,3,4]
         --
         --  Functors law 
         --  1. fmap id = id
