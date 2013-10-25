4df86e0281684d3cdcebfe283ada54f73f8d8db9
dimitar9 <dimitar9909@gmail.com>
2013-10-24 22:43:46 -0400

    functor

--- functor2.hs
+++ functor2.hs
@@ -1,3 +1,5 @@
 main = do line <- fmap reverse getLine  
           putStrLn $ "You said " ++ line ++ " backwards!"  
-          putStrLn $ "Yes, you really said" ++ line ++ " backwards!"  
+          putStrLn $ "Yes, you really said" ++ line ++ " backwards!" 
+
+        --  putStrLn <- fmap (replicate 3) [1,2,3,4]
